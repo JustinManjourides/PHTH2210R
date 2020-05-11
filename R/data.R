dcc918bfb66455b8c53499f81c436fe1fb3eefdb
Justin Manjourides <justin.manjourides@gmail.com>
2020-05-11 13:14:31 -0400

    Data descriptions

---
+++ R/data.R
@@ -0,0 +1,19 @@
+
+
+#' @title babies
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 100 rows and 10 variables:
+#' \describe{
+#'   \item{\code{headcirc}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{length}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{gestage}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{birthwt}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{momage}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{toxemia}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{sbp}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{sex}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{grmhrm}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{apgar5}}{double COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://somewhere.important.com/}
+"babies"
